5241de74804b5115e5bbb6156a4c943886fbe2da
Limchihyeon <cse.lyc3004@gmail.com>
2024-01-10 13:19:26 +0900

    Q1012

--- src/BaekJun/Silver/Q1012.java
+++ src/BaekJun/Silver/Q1012.java
@@ -5,7 +5,30 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
+
 public class Q1012 {
+
+    public static int update_flag(int[][][] loc, int x, int y, int area ,int count){
+        if(loc[x][y][0] == 1) {
+            loc[x][y][0] = 2; //flag 2 == 이미 탐색함
+            if(area==0) {loc[x][y][1] = count++;} //area 설정
+            else {loc[x][y][1] = area;}
+
+            if (x + 1 < loc.length) {
+                update_flag(loc, x + 1, y, area, count);
+            }
+            if (x - 1 >= 0) {
+                update_flag(loc, x - 1, y, area, count);
+            }
+            if (y + 1 < loc[0].length) {
+                update_flag(loc, x, y + 1, area, count);
+            }
+            if (y - 1 >= 0) {
+                update_flag(loc, x, y - 1, area, count);
+            }
+        }
+        return count;
+    }
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int T = Integer.parseInt(br.readLine());
@@ -16,13 +39,27 @@ public class Q1012 {
             int M = Integer.parseInt(st.nextToken());
             int N = Integer.parseInt(st.nextToken());
             int K = Integer.parseInt(st.nextToken());
-            int[][] loc = new int[M][N];
+            int[][][] loc = new int[M][N][2];
+            int count = 1;
             for(int j = 0 ; j < K ; j++){
                 st = new StringTokenizer(br.readLine());
-                loc[Integer.parseInt(st.nextToken())][Integer.parseInt(st.nextToken())] = 1;
+                int x = Integer.parseInt(st.nextToken());
+                int y = Integer.parseInt(st.nextToken());
+                loc[x][y][0] = 1; //flag
+                loc[x][y][1] = 0; //area
             }
-            
+
+            for(int j= 0 ; j< M ; j++){
+                for(int k = 0 ; k< N ; k++){
+                    count = update_flag(loc, j, k, 0,count);
+                }
+            }
+            ans[i] = count-1;
+        }
+        for(int i = 0 ; i<T ; i++){
+            System.out.println(ans[i]);
         }
     }
+
 
 }
