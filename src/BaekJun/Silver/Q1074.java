a3467ebb6e164d016bd170d8c02b37e1536d8a0c
Limchihyeon <cse.lyc3004@gmail.com>
2024-01-11 15:32:24 +0900

    Q1074

--- src/BaekJun/Silver/Q1074.java
+++ src/BaekJun/Silver/Q1074.java
@@ -1,4 +1,30 @@
 package BaekJun.Silver;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
 public class Q1074 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int N = Integer.parseInt(st.nextToken());
+        int r = Integer.parseInt(st.nextToken());
+        int c = Integer.parseInt(st.nextToken());
+        int cnt = 0;
+
+        while(N>0) {
+            int q = 0;
+            if(1<<(N-1) > r && 1<<(N-1) > c) {q=0;}
+            else if (1<<(N-1) > r && 1<<(N-1) <= c) {q=1; c-=1<<(N-1);}
+            else if (1<<(N-1) <= r && 1<<(N-1) > c) {q=2; r-=1<<(N-1);}
+            else {q=3; c-=1<<(N-1); r-=1<<(N-1);}
+            cnt+=q*(1<<(2*(N-1)));
+            N-=1;
+        }
+
+        System.out.println(cnt);
+    }
 }
